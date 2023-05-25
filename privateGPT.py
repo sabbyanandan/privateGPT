522d6f09f9f2c37c1b13594741dc923081490675
Sabby Anandan <sabby.anandan@gmail.com>
2023-05-25 16:47:41 -0700

    Initial updates to Docbot

--- privateGPT.py
+++ privateGPT.py
@@ -7,70 +7,110 @@ from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
 import os
 import argparse
+import flask
+
+from flask import render_template
+
+app = flask.Flask(__name__)
+
+qa = None
 
 load_dotenv()
 
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
+persist_directory = os.environ.get("PERSIST_DIRECTORY")
 
-model_type = os.environ.get('MODEL_TYPE')
-model_path = os.environ.get('MODEL_PATH')
-model_n_ctx = os.environ.get('MODEL_N_CTX')
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+model_type = os.environ.get("MODEL_TYPE")
+model_path = os.environ.get("MODEL_PATH")
+model_n_ctx = os.environ.get("MODEL_N_CTX")
+target_source_chunks = int(os.environ.get("TARGET_SOURCE_CHUNKS", 4))
 
 from constants import CHROMA_SETTINGS
 
-def main():
-    # Parse the command line arguments
-    args = parse_arguments()
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-    retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-    # activate/deactivate the streaming StdOut callback for LLMs
-    callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
-    # Prepare the LLM
-    match model_type:
-        case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
-        case "GPT4All":
-            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
-        case _default:
-            print(f"Model {model_type} not supported!")
-            exit;
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
-    # Interactive questions and answers
-    while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
 
-        # Get the answer from the chain
-        res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+@app.route("/app")
+def theapp():
+    return render_template("app.html", title="App")
 
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print("\n> Answer:")
-        print(answer)
 
-        # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+@app.route("/")
+def index():
+    return "Hello, world!"
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
-    parser.add_argument("--hide-source", "-S", action='store_true',
-                        help='Use this flag to disable printing of source documents used for answers.')
 
-    parser.add_argument("--mute-stream", "-M",
-                        action='store_true',
-                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
+@app.route("/ask/<string:s>")
+def ask(s):
+    # return "hello"
+    return my_chatGPT_bot(s)
 
-    return parser.parse_args()
+
+def init():
+    print("Okay, starting to bootstrap model!")
+    global qa
+    if qa is None:
+        embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+        db = Chroma(
+            persist_directory=persist_directory,
+            embedding_function=embeddings,
+            client_settings=CHROMA_SETTINGS,
+        )
+        retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+
+        print("model_11type = " + model_type)
+
+        match model_type:
+            case "LlamaCpp":
+                llm = LlamaCpp(
+                    model_path=model_path,
+                    n_ctx=model_n_ctx,
+                    callbacks=[],
+                    verbose=False,
+                )
+            case "GPT4All":
+                llm = GPT4All(
+                    model=model_path,
+                    n_ctx=model_n_ctx,
+                    backend="gptj",
+                    callbacks=[],
+                    verbose=False,
+                )
+            case _default:
+                print(f"Model {model_type} not supported!")
+                exit
+
+        print("Prep QA")
+
+        qa = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=retriever,
+            return_source_documents=False,
+        )
+
+        print("Okay: bootstrapping model complete!")
+    else:
+        print("Okay, Model is already bootstrapped and ready to go!")
+
+
+def my_chatGPT_bot(input_text):
+    print("QA the thing")
+    init()
+
+    res = qa(input_text)
+
+    print("Response from QA")
+    answer = res["result"]
+
+    # Print the result
+    print("\n\n> Question:")
+    print(input_text)
+    print("\n> Answer:")
+    print(answer)
+
+    return answer
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    init()
+    app.run()
